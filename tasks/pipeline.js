41edfc724e587ab4181f7af55d7b43a1129039d6
matthova <matthova87@gmail.com>
2017-06-11 01:01:16 -0700

    Include create-react-app js and css

--- tasks/pipeline.js
+++ tasks/pipeline.js
@@ -17,7 +17,8 @@
 // (if you're using LESS with the built-in default config, you'll want
 //  to change `assets/styles/importer.less` instead.)
 var cssFilesToInject = [
-  'styles/**/*.css'
+  'styles/**/*.css',
+  'build/**/*.css',
 ];
 
 
@@ -33,7 +34,9 @@ var jsFilesToInject = [
 
   // All of the rest of your client-side js files
   // will be injected here in no particular order.
-  'js/**/*.js'
+  'js/**/*.js',
+
+  'build/**/*.js',
 ];
 
 
