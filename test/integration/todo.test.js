deae7e218a039b6ae578d27845a64ed2fea02157
matthova <matthova87@gmail.com>
2017-06-11 16:33:23 -0700

    Add test script for frontend and backend

---
+++ test/integration/todo.test.js
@@ -0,0 +1,19 @@
+/* global describe, it */
+const should = require('should');
+const fs = require('fs-promise');
+const path = require('path');
+const requestPromise = require('request-promise');
+
+const request = requestPromise.defaults({ json: true });
+
+describe('Todo unit test', function () {
+  it('should create a todo', async function () {
+    const createTodoReply = await request.post({
+      uri: 'http://localhost:1337/api/todo',
+      body: {
+        description: 'just a test',
+      },
+    })
+    .catch(createTodoError => console.log(createTodoError));
+  });
+});
