346dbfa896b4ea750cf018e98e3d2f864656842d
matthova <matthova87@gmail.com>
2017-06-11 01:09:38 -0700

    Added todo model and controller

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [{description: 'one', id: 100}],
+    };
+  }
+
+  componentWillMount() {
+    if (require('is-browser')) {
+      fetch('/api/todo')
+      .then(reply => reply.json())
+      .then(todos => this.setState({ todos }));
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
+        { this.state.todos.map(todo => <p key={todo.id}>{todo.description}</p>)}
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
