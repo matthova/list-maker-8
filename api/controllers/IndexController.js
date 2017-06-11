8762e13ace5475418ca4c310e4fca98e755b68a2
matthova <matthova87@gmail.com>
2017-06-11 01:06:54 -0700

    Send all routes through the index controller first for rendering react

---
+++ api/controllers/IndexController.js
@@ -0,0 +1,30 @@
+/**
+ * IndexController
+ *
+ * @description :: Server-side logic for managing indices
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+const path = require('path');
+const fs = require('fs');
+
+const React = require('react');
+const {renderToString} = require('react-dom/server');
+const {StaticRouter} = require('react-router-dom');
+const { createElement: h } = require('react');
+const {default: App} = require('../../react/src/App')
+
+function universalLoader(req, res, next) {
+  if (req.url.length >= 4 && req.url.substr(0, 4) === '/api') {
+    return next();
+  }
+
+  const app = h(App);
+  const staticRouter = h(StaticRouter, { location: req.url, context: {} }, app);
+  const markup = renderToString(staticRouter);
+  // we're good, send the response
+  res.view('homepage', { markup });
+}
+
+module.exports = {
+  index: universalLoader,
+};
