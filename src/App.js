92d58b1fc3774bdad0af372735a67fdced068f0b
matthova <matthova87@gmail.com>
2017-06-11 16:37:51 -0700

    Bump files to start travis build

--- src/App.js
+++ src/App.js
@@ -19,9 +19,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
         { this.state.todos.map(todo => <p key={todo.id}>{todo.description}</p>)}
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
