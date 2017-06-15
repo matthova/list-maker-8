809aa3d30107456de18767760162f1a2a806f1fc
matthova <matthova87@gmail.com>
2017-06-14 22:58:06 -0700

    Modified react test to use static router surrounding the app

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,15 @@
 /* global document, it */
 import React from "react";
 import ReactDOM from "react-dom";
+import { StaticRouter } from "react-router-dom";
 import App from "./App";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
+  const appAndRouter = React.createElement(
+    StaticRouter,
+    { context: {} },
+    <App />
+  );
+  ReactDOM.render(appAndRouter, div);
 });
